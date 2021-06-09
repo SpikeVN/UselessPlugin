523b073d5e220ea603378e0ecd5e8accddff8f55
SpikeBonjor <triphuongvasa@gmail.com>
2021-06-09 11:13:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UselessPlugin
+ My sh*tty code for you to know how bad at coding I am.
---
+++ src/main/java/me/spike/mymoreuselessplugin/MyMoreUselessPlugin.java
@@ -0,0 +1,112 @@
+package me.spike.mymoreuselessplugin;
+
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Locale;
+
+public final class MyMoreUselessPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        System.out.println("[UselessPlugin] Plugin has started successfully.");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[UselessPlugin] Deactivating...");
+        System.out.println("[UselessPlugin] Goodbye! :)");
+    }
+
+    @EventHandler
+    public void onSleep(PlayerBedLeaveEvent event) {
+        Player sleeper = event.getPlayer();
+        World world = sleeper.getWorld();
+        if (world.getTime() >= 13000 && world.getTime() <= 23000) {
+            world.setTime(23000);
+            Bukkit.broadcastMessage(ChatColor.GOLD + sleeper.getName() + ChatColor.YELLOW + " đã ngủ. Chúc ngủ ngon!");
+        }
+    }
+
+    @EventHandler
+    public void onOpenInv(InventoryOpenEvent event) {
+        Player victim = (Player) event.getPlayer();
+        if (victim.getGameMode() == GameMode.ADVENTURE) {
+            victim.setHealth(0);
+            Bukkit.broadcastMessage(ChatColor.RED + victim.getName() + " đã không làm gì cả nên chúng tôi giết anh ta cho vui.");
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String cmd = command.getName().toUpperCase(Locale.ROOT);
+        if (cmd.equals("BLOW")) {
+            try {
+                blow(sender, NameToPlayer(args[1]));
+            } catch (Exception x) {
+                ExceptionHandler(x, "BlowCommand", (Player) sender);
+            }
+
+        }
+        if (cmd.equals("VODAU")) {
+            try {
+                crackHead(NameToPlayer(args[1]));
+            } catch (Exception wtf) {
+                ExceptionHandler(wtf, "CrackHeadCommand", (Player) sender);
+            }
+        }
+        if (cmd.equals("MUP")) {
+            try {
+                    sender.sendMessage(ChatColor.GREEN + "[UselessPlugin] Debug mode started.");
+                    sender.sendMessage(ChatColor.GREEN + "[UselessPlugin] This mode is for the idiot who wants to debug his bullshit plugin.");
+                    sender.sendMessage(ChatColor.GREEN + "[UselessPlugin] Debug Info:");
+                    sender.sendMessage(ChatColor.GOLD + "Sender: " + sender.getName() + ", command: " + command.getName() + ", label: " + label);
+                    sender.sendMessage(ChatColor.GOLD + "String [] args Array Value:");
+                    for (String a : args) {
+                        sender.sendMessage(a);
+                    }
+                    sender.sendMessage("Report Complete.");
+            } catch (Exception z) {
+                ExceptionHandler(z, "DEBUG", (Player) sender);
+            }
+
+            }
+
+        return true;
+    }
+
+    public static void ExceptionHandler(Exception x, String pos, Player sender) {
+        System.out.println("[UselessPlugin] " + ChatColor.RED + "An error occured at " + pos);
+        System.out.println("[UselessPlugin] " + ChatColor.YELLOW + x);
+        sender.sendMessage(ChatColor.RED + "An error occurred: " + pos + x);
+    }
+
+    public static void blow(CommandSender sender, Player victim) {
+        victim.getWorld().spawnEntity(victim.getLocation(), EntityType.PRIMED_TNT);
+    }
+
+    public static void crackHead(Player victim) {
+        World world = victim.getWorld();
+        Location location = victim.getLocation().add(0,2,0);
+        world.spawnFallingBlock(location, Material.ANVIL, (byte) 0);
+    }
+
+    public static Player NameToPlayer(String name) {
+        Player victim = null;
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.getName().equals(name)) {
+                victim = player;
+            }
+        }
+        return victim;
+    }
+}
